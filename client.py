4288de70b18f01a1c2b9ecec1349e820bd7180f5
Ilya <iliamikado@gmail.com>
2025-11-07 17:21:26 +0200

    New tests

--- client.py
+++ client.py
@@ -9,13 +9,15 @@ def test_existing_page():
     print("Check existing page 'hello'")
     resp = requests.get(f"{url}/hello")
     if resp.status_code != 200:
-        print(f"{ERR}Response status isn't 200{RST}")
+        print(f"{ERR}Response status isn't 200")
+        print(f"Got {resp.status_code}{RST}")
         sys.exit(1)
     else:
         print(f"{OK}Response code is 200{RST}")
 
     if resp.content != b"Hello, World!":
-        print(f"{ERR}Response content not equal b'Hello, World!'{RST}")
+        print(f"{ERR}Response content not equal b'Hello, World!'")
+        print(f"Got {resp.content}{RST}")
         sys.exit(1)
     else:
         print(f"{OK}Response content is right{RST}")
@@ -24,7 +26,8 @@ def test_unexisting_page():
     print("Check unexisting page")
     resp = requests.get(f"{url}/ssss")
     if resp.status_code != 404:
-        print(f"{ERR}Response status isn't 404{RST}")
+        print(f"{ERR}Response status isn't 404")
+        print(f"Got {resp.status_code}{RST}")
         sys.exit(1)
     else:
         print(f"{OK}Response status is 404{RST}")
