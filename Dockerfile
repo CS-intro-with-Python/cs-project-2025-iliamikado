87250abb76d4f8172f0451080c2f47bc1008ad07
Ilya <iliamikado@gmail.com>
2025-11-07 17:18:31 +0200

    New tests

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.11-slim
 WORKDIR /app
 COPY . /app
+
+# ENV FLASK_RUN_RELOAD=true
+
 RUN pip install -r requirements.txt
-CMD ["python", "server.py"]
+CMD ["flask", "--app", "server.py", "run", "-h", "0.0.0.0", "-p", "5000"]
--- client.py
+++ client.py
@@ -1,7 +1,34 @@
 import sys
 import requests
 
-resp = requests.get("http://localhost:5000/")
+OK, ERR, RST = "\033[32m", "\033[31m", "\033[0m"
 
-if resp.status_code != 200:
-    sys.exit(1)
+url = "http://localhost:5000"
+
+def test_existing_page():
+    print("Check existing page 'hello'")
+    resp = requests.get(f"{url}/hello")
+    if resp.status_code != 200:
+        print(f"{ERR}Response status isn't 200{RST}")
+        sys.exit(1)
+    else:
+        print(f"{OK}Response code is 200{RST}")
+
+    if resp.content != b"Hello, World!":
+        print(f"{ERR}Response content not equal b'Hello, World!'{RST}")
+        sys.exit(1)
+    else:
+        print(f"{OK}Response content is right{RST}")
+
+def test_unexisting_page():
+    print("Check unexisting page")
+    resp = requests.get(f"{url}/ssss")
+    if resp.status_code != 404:
+        print(f"{ERR}Response status isn't 404{RST}")
+        sys.exit(1)
+    else:
+        print(f"{OK}Response status is 404{RST}")
+
+
+test_existing_page()
+test_unexisting_page()
